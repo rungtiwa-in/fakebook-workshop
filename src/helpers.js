e0b8d7da350243504fa0bd024e3f2207b1994ecc
Rungtiwa Inha <rungtiwa.in@baac.or.th>
2020-07-29 09:35:37 +0700

    update

--- src/helpers.js
+++ src/helpers.js
@@ -3,6 +3,11 @@ export function GetName() {
   return user ? user.fullName : "";
 }
 
+export function GetToken() {
+  const user = JSON.parse(localStorage.getItem("user"));
+  return user ? user.token : "";
+}
+
 export function Logout() {
   localStorage.clear();
 }
